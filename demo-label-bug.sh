4aa8377e603ec3b556ae0120ce8757617b74f896
Steve Cresswell <229672+cressie176@users.noreply.github.com>
2025-01-17 20:02:28 +0000

    Debug Build

--- demo-label-bug.sh
+++ demo-label-bug.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
+set -x
 
+OWNER=acuminous
+REPO=knuff-github-driver
 ID=$(openssl rand -base64 12)
 
 curl -X POST \
 -H "Authorization: token ${GITHUB_TOKEN}" \
 -H "Accept: application/vnd.github+json" \
-https://api.github.com/repos/acuminous/knuff-github-driver/issues \
+https://api.github.com/repos/${OWNER}/${REPO}/issues \
 -d "{
   \"title\": \"Label Bug\",
   \"body\": \"Demonstrates the Label Bug\",
-  \"labels\": [\"l1\", \"l2\", \"knuff:test-${ID}\", \"knuff:2024-12-25\"]
+  \"labels\": [\"knuff:test-${ID}\"]
 }"
