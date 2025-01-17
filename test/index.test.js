363d59477270c8871ce2cd1af8b44ada2e5b8d0a
Steve Cresswell <229672+cressie176@users.noreply.github.com>
2025-01-17 19:02:20 +0000

    Debug Build

--- test/index.test.js
+++ test/index.test.js
@@ -22,7 +22,7 @@ describe('driver', () => {
   it('should create issues with labels', async (t) => {
     const repository = { owner: 'acuminous', repo: 'knuff-github-driver' };
     const reminder = {
-      id: `test-${crypto.randomBytes(10).toString('hex')}`,
+      id: `test1`,
       title: t.name,
       body: 'the body',
       labels: ['l1', 'l2'],
@@ -42,129 +42,129 @@ describe('driver', () => {
     eq(labels[3], 'l2');
   });
 
-  it('should create issues without labels', async (t) => {
-    const repository = { owner: 'acuminous', repo: 'knuff-github-driver' };
-    const reminder = {
-      id: `test-${crypto.randomBytes(10).toString('hex')}`,
-      title: t.name,
-      body: 'the body',
-      date: new Date('2024-12-25'),
-      timezone: 'Europe/London',
-    };
-    const { data: issue } = await driver.createReminder(repository, reminder);
+  // it('should create issues without labels', async (t) => {
+  //   const repository = { owner: 'acuminous', repo: 'knuff-github-driver' };
+  //   const reminder = {
+  //     id: `test-${crypto.randomBytes(10).toString('hex')}`,
+  //     title: t.name,
+  //     body: 'the body',
+  //     date: new Date('2024-12-25'),
+  //     timezone: 'Europe/London',
+  //   };
+  //   const { data: issue } = await driver.createReminder(repository, reminder);
 
-    ok(issue.number);
-    eq(issue.title, 'should create issues without labels');
-    eq(issue.labels.length, 2);
+  //   ok(issue.number);
+  //   eq(issue.title, 'should create issues without labels');
+  //   eq(issue.labels.length, 2);
 
-    const labels = issue.labels.map((l) => l.name).sort();
-    eq(labels[0], 'knuff:2024-12-25');
-    eq(labels[1], `knuff:${reminder.id}`);
-  });
+  //   const labels = issue.labels.map((l) => l.name).sort();
+  //   eq(labels[0], 'knuff:2024-12-25');
+  //   eq(labels[1], `knuff:${reminder.id}`);
+  // });
 
-  it('should honour timezones', async (t) => {
-    const repository = { owner: 'acuminous', repo: 'knuff-github-driver' };
-    const reminder = {
-      id: `test-${crypto.randomBytes(10).toString('hex')}`,
-      title: t.name,
-      body: 'the body',
-      labels: ['l1', 'l2'],
-      date: new Date('2024-12-25T13:00:00Z'),
-      timezone: 'Australia/Sydney',
-    };
-    const { data: issue } = await driver.createReminder(repository, reminder);
+  // it('should honour timezones', async (t) => {
+  //   const repository = { owner: 'acuminous', repo: 'knuff-github-driver' };
+  //   const reminder = {
+  //     id: `test-${crypto.randomBytes(10).toString('hex')}`,
+  //     title: t.name,
+  //     body: 'the body',
+  //     labels: ['l1', 'l2'],
+  //     date: new Date('2024-12-25T13:00:00Z'),
+  //     timezone: 'Australia/Sydney',
+  //   };
+  //   const { data: issue } = await driver.createReminder(repository, reminder);
 
-    ok(issue.number);
-    eq(issue.title, 'should honour timezones');
-    eq(issue.labels.length, 4);
+  //   ok(issue.number);
+  //   eq(issue.title, 'should honour timezones');
+  //   eq(issue.labels.length, 4);
 
-    const labels = issue.labels.map((l) => l.name).sort();
-    eq(labels[0], 'knuff:2024-12-26');
-    eq(labels[1], `knuff:${reminder.id}`);
-    eq(labels[2], 'l1');
-    eq(labels[3], 'l2');
-  });
+  //   const labels = issue.labels.map((l) => l.name).sort();
+  //   eq(labels[0], 'knuff:2024-12-26');
+  //   eq(labels[1], `knuff:${reminder.id}`);
+  //   eq(labels[2], 'l1');
+  //   eq(labels[3], 'l2');
+  // });
 
-  it('should reject reminders with labels greater than 50 characters', async (t) => {
-    const repository = { owner: 'acuminous', repo: 'knuff-github-driver' };
-    const reminder = {
-      id: `test-${crypto.randomBytes(10).toString('hex')}`,
-      title: t.name,
-      body: 'the body',
-      labels: ['123456789 123456789 123456789 123456789 123456789 1'],
-      date: new Date('2024-12-25'),
-    };
+  // it('should reject reminders with labels greater than 50 characters', async (t) => {
+  //   const repository = { owner: 'acuminous', repo: 'knuff-github-driver' };
+  //   const reminder = {
+  //     id: `test-${crypto.randomBytes(10).toString('hex')}`,
+  //     title: t.name,
+  //     body: 'the body',
+  //     labels: ['123456789 123456789 123456789 123456789 123456789 1'],
+  //     date: new Date('2024-12-25'),
+  //   };
 
-    await rejects(() => driver.createReminder(repository, reminder), (error) => {
-      eq(error.message, "Label '123456789 123456789 123456789 123456789 123456789 1' is longer than the GitHub max length of 50 characters");
-      return true;
-    });
-  });
+  //   await rejects(() => driver.createReminder(repository, reminder), (error) => {
+  //     eq(error.message, "Label '123456789 123456789 123456789 123456789 123456789 1' is longer than the GitHub max length of 50 characters");
+  //     return true;
+  //   });
+  // });
 
-  it('should reject reminders with an id greater than 46 characters', async (t) => {
-    const repository = { owner: 'acuminous', repo: 'knuff-github-driver' };
-    const reminder = {
-      id: '123456789 123456789 123456789 123456789 12345',
-      title: t.name,
-      body: 'the body',
-      date: new Date('2024-12-25'),
-    };
+  // it('should reject reminders with an id greater than 46 characters', async (t) => {
+  //   const repository = { owner: 'acuminous', repo: 'knuff-github-driver' };
+  //   const reminder = {
+  //     id: '123456789 123456789 123456789 123456789 12345',
+  //     title: t.name,
+  //     body: 'the body',
+  //     date: new Date('2024-12-25'),
+  //   };
 
-    await rejects(() => driver.createReminder(repository, reminder), (error) => {
-      eq(error.message, "Label 'knuff:123456789 123456789 123456789 123456789 12345' is longer than the GitHub max length of 50 characters");
-      return true;
-    });
-  });
+  //   await rejects(() => driver.createReminder(repository, reminder), (error) => {
+  //     eq(error.message, "Label 'knuff:123456789 123456789 123456789 123456789 12345' is longer than the GitHub max length of 50 characters");
+  //     return true;
+  //   });
+  // });
 
-  it('should find open issues with the same id and date', async (t) => {
-    const repository = { owner: 'acuminous', repo: 'knuff-github-driver' };
-    const reminder = {
-      id: `test-${crypto.randomBytes(10).toString('hex')}`,
-      title: t.name,
-      body: 'the body',
-      labels: ['l1', 'l2'],
-      date: new Date('2024-12-25'),
-    };
+  // it('should find open issues with the same id and date', async (t) => {
+  //   const repository = { owner: 'acuminous', repo: 'knuff-github-driver' };
+  //   const reminder = {
+  //     id: `test-${crypto.randomBytes(10).toString('hex')}`,
+  //     title: t.name,
+  //     body: 'the body',
+  //     labels: ['l1', 'l2'],
+  //     date: new Date('2024-12-25'),
+  //   };
 
-    await driver.createReminder(repository, reminder);
+  //   await driver.createReminder(repository, reminder);
 
-    const found = await driver.hasReminder(repository, reminder);
-    eq(found, true);
-  });
+  //   const found = await driver.hasReminder(repository, reminder);
+  //   eq(found, true);
+  // });
 
-  it('should find closed issues with the same id and date', async (t) => {
-    const repository = { owner: 'acuminous', repo: 'knuff-github-driver' };
-    const reminder = {
-      id: `test-${crypto.randomBytes(10).toString('hex')}`,
-      title: t.name,
-      body: 'the body',
-      labels: ['l1', 'l2'],
-      date: new Date('2024-12-25'),
-    };
+  // it('should find closed issues with the same id and date', async (t) => {
+  //   const repository = { owner: 'acuminous', repo: 'knuff-github-driver' };
+  //   const reminder = {
+  //     id: `test-${crypto.randomBytes(10).toString('hex')}`,
+  //     title: t.name,
+  //     body: 'the body',
+  //     labels: ['l1', 'l2'],
+  //     date: new Date('2024-12-25'),
+  //   };
 
-    const { data: issue } = await driver.createReminder(repository, reminder);
-    await closeIssue(issue.number);
+  //   const { data: issue } = await driver.createReminder(repository, reminder);
+  //   await closeIssue(issue.number);
 
-    const found = await driver.hasReminder(repository, reminder);
-    eq(found, true);
-  });
+  //   const found = await driver.hasReminder(repository, reminder);
+  //   eq(found, true);
+  // });
 
-  it('should ignore issues with the same id but different day', async (t) => {
-    const repository = { owner: 'acuminous', repo: 'knuff-github-driver' };
-    const reminder = {
-      id: `test-${crypto.randomBytes(10).toString('hex')}`,
-      title: t.name,
-      body: 'the body',
-      labels: ['l1', 'l2'],
-      date: new Date('2024-12-25'),
-      timezone: 'Europe/London',
-    };
+  // it('should ignore issues with the same id but different day', async (t) => {
+  //   const repository = { owner: 'acuminous', repo: 'knuff-github-driver' };
+  //   const reminder = {
+  //     id: `test-${crypto.randomBytes(10).toString('hex')}`,
+  //     title: t.name,
+  //     body: 'the body',
+  //     labels: ['l1', 'l2'],
+  //     date: new Date('2024-12-25'),
+  //     timezone: 'Europe/London',
+  //   };
 
-    await driver.createReminder(repository, reminder);
+  //   await driver.createReminder(repository, reminder);
 
-    const found = await driver.hasReminder(repository, { ...reminder, date: new Date('2024-12-26') });
-    eq(found, false);
-  });
+  //   const found = await driver.hasReminder(repository, { ...reminder, date: new Date('2024-12-26') });
+  //   eq(found, false);
+  // });
 
   async function nuke() {
     const issues = await listOpenIssues();
