d04d2440a06b988f31f19382176ff5ca287a10bd
Steve Cresswell <229672+cressie176@users.noreply.github.com>
2025-01-17 19:11:52 +0000

    Debug Build

--- test/index.test.js
+++ test/index.test.js
@@ -22,7 +22,7 @@ describe('driver', () => {
   it('should create issues with labels', async (t) => {
     const repository = { owner: 'acuminous', repo: 'knuff-github-driver' };
     const reminder = {
-      id: `test-a3e780fd40034e1502fa`,
+      id: `test-${crypto.randomBytes(10).toString('hex')}`,
       title: t.name,
       body: 'the body',
       labels: ['l1', 'l2'],
